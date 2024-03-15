246d8045d9490e6c0649d1116b50d99a067cda79
Tim Hamp <timrouven@gmx.net>
2024-03-15 23:10:56 +0100

    Started with tests #2

--- src/locations/locations.service.spec.ts
+++ src/locations/locations.service.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationsService } from './locations.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Location } from './entities/location.entity';
+import { EntityManager, Repository } from 'typeorm';
 
 describe('LocationService', () => {
   let service: LocationsService;
+  let locationsRepository: Repository<Location>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LocationsService],
+      providers: [
+        LocationsService,
+        {
+          provide: getRepositoryToken(Location),
+          useValue: {
+            find: jest.fn(),
+          },
+        },
+        {
+          provide: EntityManager,
+          useValue: {},
+        }
+      ],
     }).compile();
 
     service = module.get<LocationsService>(LocationsService);
+    locationsRepository = module.get<Repository<Location>>(getRepositoryToken(Location))
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  test('findAll', async () => {
+    await service.findAll();
+    expect(locationsRepository.find).toHaveBeenCalled();
+  })
 });
